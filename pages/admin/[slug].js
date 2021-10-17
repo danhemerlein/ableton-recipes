e3fcd1835eebc2dc6ccf3eb2c3f3c5e902fae7bc
Dan Hemerlein <danhemerlein@gmail.com>
2021-10-17 15:02:10 -0700

    bein edit form development

--- pages/admin/[slug].js
+++ pages/admin/[slug].js
@@ -1,8 +1,72 @@
+import { doc, onSnapshot } from '@firebase/firestore';
+import Link from 'next/Link';
+import { useRouter } from 'next/router';
+import { useEffect, useState } from 'react';
+import AuthCheck from '../../components/AuthCheck';
+import { auth, firestore, getPostByUserAndSlug } from '../../lib/firebase';
+
 const AdminSlug = ({}) => {
   return (
-    <>
-      <h1>Admin slug</h1>
-    </>
+    <AuthCheck>
+      <PostManager />
+    </AuthCheck>
   );
 };
+
 export default AdminSlug;
+
+function PostManager() {
+  const [preview, setPreview] = useState(false);
+  const [post, setPost] = useState([]);
+  const [postRef, setPostRef] = useState(null);
+
+  const router = useRouter();
+  const { slug } = router.query;
+
+  useEffect(async () => {
+    // turn off realtime subscription
+    let unsubscribe;
+
+    const userRef = doc(firestore, 'users', auth.currentUser.uid);
+
+    const { postData, refData } = await getPostByUserAndSlug(userRef, slug);
+
+    setPost(postData);
+    setPostRef(refData);
+
+    unsubscribe = onSnapshot(refData, (doc) => {
+      setPost(doc.data());
+    });
+
+    return unsubscribe;
+  }, []);
+
+  return (
+    <main>
+      {post && (
+        <>
+          <section>
+            <h1>{post.title}</h1>
+            <p>ID: {post.slug}</p>
+
+            {/* <PostForm
+              postRef={postRef}
+              defaultValues={post}
+              preview={preview}
+            /> */}
+          </section>
+
+          <aside>
+            <h3>Tools</h3>
+            <button onClick={() => setPreview(!preview)}>
+              {preview ? 'Edit' : 'Preview'}
+            </button>
+            <Link href={`/${post.username}/${post.slug}`}>
+              <button className="btn-blue">Live view</button>
+            </Link>
+          </aside>
+        </>
+      )}
+    </main>
+  );
+}
